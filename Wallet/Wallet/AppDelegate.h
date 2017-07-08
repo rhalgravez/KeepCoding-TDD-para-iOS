b9e881c984e25ba1dd06c8996e538acba15544c0
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-07-07 19:25:12 -0600

    Create an empty project called Wallet with the Unit Tests included

---
+++ Wallet/Wallet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wallet
+//
+//  Created by Roberto Halgravez on 7/7/17.
+//  Copyright © 2017 Roberto Halgravez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Wallet/Wallet/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Wallet
+//
+//  Created by Roberto Halgravez on 7/7/17.
+//  Copyright © 2017 Roberto Halgravez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

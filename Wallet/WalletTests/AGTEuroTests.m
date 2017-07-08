e06da562850232b90669d3c9e7e53a26259725f5
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-07-07 19:54:09 -0600

    Create our first test (testMultiplication)
    1)Put what we are going to test. In this case the class that we doesn't have yet that is going to handle the euro multiplication
    
    2)Put the XCTAssert. In this case we use the XCTAssertEqual because we are going to compare a number (not an object)
    
    3)Run the test. The test should fail becuase we don't have yet the AGTEuro class

--- Wallet/WalletTests/AGTEuroTests.m
+++ Wallet/WalletTests/AGTEuroTests.m
@@ -14,26 +14,11 @@
 
 @implementation AGTEuroTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+-(void)testMultiplication {
+    AGTEuro *euro = [[AGTEuro alloc] initWithAmount:5];
+    [euro times:2];
+    
+    XCTAssertEqual(euro.amount, 10, @"5*2 should be 10"); 
 }
 
 @end

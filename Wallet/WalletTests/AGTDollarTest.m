25f3d23764464241cf7ef3ef0e1fa8abd4039eaa
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-07-10 14:34:21 -0600

    Add the eqaulity test for Dollars just to be extensive with our tests

--- Wallet/WalletTests/AGTDollarTest.m
+++ Wallet/WalletTests/AGTDollarTest.m
@@ -24,4 +24,15 @@
     XCTAssertEqualObjects(total, ten, @"$5*2 = $10");
 }
 
+-(void)testEquality {
+    AGTDollar *five = [[AGTDollar alloc] initWithAmount:5];
+    AGTDollar *total = [five times:3];
+    
+    XCTAssertEqualObjects(total,
+                          [[AGTDollar alloc] initWithAmount:15],
+                          @"Equivalent objects should be equal");
+    
+    XCTAssertFalse([total isEqual:five], @"Non equivalent objects should not be equal");
+}
+
 @end

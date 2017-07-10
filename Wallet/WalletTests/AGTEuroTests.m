827288a28b0f20b9bb9b18363a5c8db2f972706c
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-07-10 13:31:37 -0600

    Add test method for equality comparation
    The method doesn't pass because we have to overwrite the isEqual: method

--- Wallet/WalletTests/AGTEuroTests.m
+++ Wallet/WalletTests/AGTEuroTests.m
@@ -22,4 +22,16 @@
     XCTAssertEqual(total.amount, 10, @"5*2 should be 10");
 }
 
+//Ahora que modifiqué el modelo para ser inmutable debo tener la capacidad de hacer comparaciones
+//entre obetos, por eso creamos el test de igualdad
+-(void)testEquality {
+    //Buena práctica: Creamos dos objetos que representan la misma cantidad pero creados de forma distinta
+    AGTEuro *five = [[AGTEuro alloc] initWithAmount:5];
+    AGTEuro *ten = [[AGTEuro alloc] initWithAmount:10];
+    
+    AGTEuro *total = [five times:2];
+    
+    XCTAssertEqualObjects(ten, total, @"Equivalent object should be equal");
+}
+
 @end

a77e82b78fef7e3698e509ddb995b5ca58738c65
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-07-10 14:08:04 -0600

    Add a new test class for Dollars
    Now we are going to test if 5USD * 2 is equal to 10USD
    
    We cannot make the test for €5 + $10 because we don't have yet the dollar class
    that's why we are creating a new class test for Dollars

---
+++ Wallet/WalletTests/AGTDollarTest.m
@@ -0,0 +1,19 @@
+//
+//  AGTDollarTest.m
+//  Wallet
+//
+//  Created by Roberto Halgravez on 7/10/17.
+//  Copyright © 2017 Roberto Halgravez. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AGTDollarTest : XCTestCase
+
+@end
+
+@implementation AGTDollarTest
+
+
+
+@end

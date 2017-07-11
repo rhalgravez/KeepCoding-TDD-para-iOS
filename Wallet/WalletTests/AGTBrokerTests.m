9863cf396c52910cf96292c29a2beeffeca1a753
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-07-11 11:56:49 -0600

    Add a new test class where we are going to convert the currency
    Example $10 = €5

---
+++ Wallet/WalletTests/AGTBrokerTests.m
@@ -0,0 +1,27 @@
+//
+//  AGTBrokerTests.m
+//  Wallet
+//
+//  Created by Roberto Halgravez on 7/11/17.
+//  Copyright © 2017 Roberto Halgravez. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AGTBrokerTests : XCTestCase
+
+@end
+
+@implementation AGTBrokerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+@end

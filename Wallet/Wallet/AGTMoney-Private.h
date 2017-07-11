d56b69dcf7a5ab9f49d285a0c0c7fadb6bca52ed
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-07-10 20:21:01 -0600

    Create a extension for AGTMoney called AGTMoney-Private in a new header file. This header file we are going to import it in AGTMoney and its subclasses

---
+++ Wallet/Wallet/AGTMoney-Private.h
@@ -0,0 +1,13 @@
+//
+//  AGTMoney-Private.h
+//  Wallet
+//
+//  Created by Roberto Halgravez on 7/10/17.
+//  Copyright © 2017 Roberto Halgravez. All rights reserved.
+//
+
+@interface AGTMoney ()
+
+@property (nonatomic) NSInteger amount;
+
+@end

4e88695b4a8e8a01d8df70a80637fced746c0234
Roberto Halgravez <roberto.halgravez@hulu.com>
2017-07-11 21:10:22 -0600

    Add test example for singleton
    In this case for NSNotificationCenter

---
+++ Wallet/WalletTests/AGTNSNtificationCenterTests.m
@@ -0,0 +1,40 @@
+//
+//  AGTNSNtificationCenterTests.m
+//  Wallet
+//
+//  Created by Roberto Halgravez on 7/11/17.
+//  Copyright © 2017 Roberto Halgravez. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AGTNSNtificationCenterTests : XCTestCase
+
+@end
+
+@implementation AGTNSNtificationCenterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+-(void)testThatSubscribesToMemoryWarning {
+    AGTFakeNotificationCenter *fake = [AGTFakeNotificationCenter new];
+    
+    AGTBigAndFatObject *fat = [AGTBigAndFatObject new];
+    
+    [fat subscribeToMemoryWarning:fake];
+    
+    NSDictionary *obs = [fake observers];
+    id observer = [obs objectForKey:UIApplicationDidReceiveMemoryWarningNotification];
+    
+    XCTAssertEqualObjects(observer, fat, @"Fat object must subscribe to UIApplicationDidReceiveMemoryWarningNotification");
+}
+
+@end
